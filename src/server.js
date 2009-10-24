11d1848e5aad943e4b90a9f622a077e6c6c25640
Atsushi Takayama <atsushi@utopia.local>
2009-10-24 03:20:35 -0400

    no more using filesystem. maybe re-add later

--- src/server.js
+++ src/server.js
@@ -2,65 +2,36 @@
  * server.js : public domain
  */
 
-var debug = true;
+var debug = false;
 function log(text){
   if (debug) opera.postError(text);
 }
 
 /* utils */
-var dir = opera.io.filesystem.mountSystemDirectory('storage');
-function readFile(filename) {
-  var data = null;
-  try {
-    var stream = dir.open(dir.resolve('/' + filename), opera.io.filemode.READ);
-    data = stream.read(100000000);
-  } catch(e) {
-    log(e);
-  } finally {
-    stream.close();
-  }
-  return data;
-};
-
-function saveFile(filename, text) {
-  try {
-    var stream = dir.open(dir.resolve('/' + filename), opera.io.filemode.WRITE);
-    stream.write(text);
-  } catch(e) {
-    log(e);
-  } finally {
-    stream.close();
-  }
-}
-
-function fileOlderThan(filename, hours) {
-  var file = dir.resolve('/' + filename);
-  if (!file.exists) return false;
-  if (new Date - file.created > hours*60*60*1000) return true;
-  return false;
-}
-
-function httpGet(url, onload, onerror) {
+function httpGet(url) {
+  var result = null;
   var xhr = new XMLHttpRequest;
   xhr.open('GET', url, false);
-  if (onload) xhr.onload = function(){onload(xhr.responseText)}
-  if (onerror) xhr.onerror = function(){onerror()}
+  xhr.onload = function(){result = xhr.responseText};
   xhr.send(null);
+  return result;
 }
 
 /* server */
 // dispatcher
-var webserver = opera.io.webserver;
 window.onload = function () {
+  var webserver = opera.io.webserver;
   if (webserver){
     webserver.addEventListener('_index', index, false);
     webserver.addEventListener('get', get,false);
+    update_siteinfo();
   }
 }
 
-var siteinfo = null;  // Array of [compiled url-regexp, info]
-var siteinfo_wildcard = [];
+var siteinfo = [];  // Array of [compiled url-regexp, info]
+var microformats = [];
 var cache = {};
+var last_updated = null;
 
 // request handler (view)
 function get(e) {
@@ -74,10 +45,11 @@ function get(e) {
   var info = search_siteinfo(url);
   log('SITEINFO searching done : ' + (new Date - startTime) + 'ms');
   log(info.map(function(e){return [e.url.toString(), e.nextLink, e.pageElement].join(' , ')}).join('\n'));
-  info = info.concat(siteinfo_wildcard);
+  info = info.concat(microformats);
 
   var res = e.connection.response;
   res.setResponseHeader('Content-type', 'application/javascript; charset=utf-8');
+  if (last_updated) res.setResponseHeader('Last-Modified', last_updated.toUTCString());
   res.write('window.AutoPagerizeCallbackSiteinfo(' + JSON.stringify(info) + ');');
   res.close();
 }
@@ -97,8 +69,8 @@ function index(e) {
 }
 
 // controller
-function search_siteinfo(url) {
-  update_siteinfo();
+function search_siteinfo(url, force_update) {
+  if (force_update || !siteinfo) update_siteinfo();
   if (cache[url]) return cache[url];
   var results = [];
   var n = siteinfo.length;
@@ -106,42 +78,32 @@ function search_siteinfo(url) {
     var re = siteinfo[n][0];
     if (re.test(url)) results.push(siteinfo[n][1]);
   }
-  cache[url] = results;
+  if (url.indexOf('?') < 0) cache[url] = results;
   return results;
 }
 
 function update_siteinfo() {
-  var filename = 'wedataAutoPagerizeSITEINFO.js';
-  var fileOld = fileOlderThan(filename, 24);
-  if (!siteinfo || fileOld) {
-    log('updating siteinfo');
-    // first try readinf file, then try xhr
-    var text = readFile(filename);
-    if (!text || fileOld) {
-      httpGet('http://ss-o.net/json/wedataAutoPagerizeSITEINFO.js', 
-        function(t){if(t.length > 1000){text = t; saveFile(filename, t)}}); // if text length is less than 1000 bytes, it maybe an error message
-    }
-    if (!text) retrun;
-    window.AutoPagerizeCallbackSiteinfo = function(ary) {
-      // reset cache
-      siteinfo = [];
-      siteinfo_wildcard = [];
-      cache = {};
+  // reset cache
+  siteinfo = [];
+  microformats = [];
+  cache = {};
 
-      var n = ary.length;
-      while(--n) {
-        var info = ary[n].data || ary[n];
-        var re = new RegExp(info.url);
-        if (re.test('http://a')) {
-          // isolate siteinfo that matches any url
-          siteinfo_wildcard.push(info);
-        } else {
-          siteinfo.push([re, info]);
-        }
-      }
-    };
-    var startTime = new Date;
-    eval(text); // loads jsonp like : AutoPagerizeCallbackSiteinfo([ /* blah blah */ ]);
-    log('SITEINFO initial scan done : ' + (new Date - startTime) + 'ms');
+  log('updating siteinfo');
+  // first try readinf file, then try xhr
+  json = httpGet('http://ss-o.net/json/wedataAutoPagerizeSITEINFO.json');
+  if (!json) return;
+  var startTime = last_updated = new Date;
+  var _siteinfo = JSON.parse(json);
+  var n = _siteinfo.length;
+  while(--n) {
+    var info = _siteinfo[n].data || _siteinfo[n];
+    var re = new RegExp(info.url);
+    if (re.test('http://a')) {
+      // isolate siteinfo that matches any url
+      microformats.push(info);
+    } else {
+      siteinfo.push([re, info]);
+    }
   }
+  log('SITEINFO initial scan done : ' + (new Date - startTime) + 'ms');
 }
